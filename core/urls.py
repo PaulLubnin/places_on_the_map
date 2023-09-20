0916615711c41893443351a6ffde037cd625f34b
paulego <egopaul@inbox.ru>
2023-09-20 10:03:42 +0500

    Renamed the "afisha" app to "places".

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from afisha.views import Index
+from places.views import Index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
