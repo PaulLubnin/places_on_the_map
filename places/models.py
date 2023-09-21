22697d659ca3027fefc6d8705a6a1b183c1ed4ce
paulego <egopaul@inbox.ru>
2023-09-21 11:03:07 +0500

    Created translation of the admin.

--- places/models.py
+++ places/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class EventOrganizer(models.Model):
+    """Организаторы мероприятий."""
+
+    title = models.CharField(
+        'Title',
+        max_length=128,
+        unique=True
+    )
+    short_description = models.TextField(
+        'Short description',
+        max_length=512,
+        blank=True
+    )
+    long_description = models.TextField(
+        'Long description',
+        blank=True
+    )
+    longitude = models.FloatField(
+        'Longitude',
+        default=0
+    )
+    latitude = models.FloatField(
+        'Latitude',
+        default=0
+    )
+
+    class Meta:
+        verbose_name = 'Организатор'
+        verbose_name_plural = 'Организаторы'
+
+    def __str__(self):
+        return self.title
+
+    def coordinates(self):
+        """Координаты."""
+
+        coordinates = (self.longitude, self.latitude)
+        return coordinates
+
