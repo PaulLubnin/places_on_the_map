2c672d24c94c936683eb6185a4bdc07c491d82a1
paulego <egopaul@inbox.ru>
2023-10-18 18:18:06 +0500

    from home

--- places/views.py
+++ places/views.py
@@ -1,5 +1,38 @@
+from django.http import JsonResponse
 from django.views.generic import TemplateView
+
+from places.models import EventOrganizer
 
 
 class Index(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = {}
+        context['type'] = 'FeatureCollection'
+        context['features'] = []
+        organizers = EventOrganizer.objects.all()
+        for elem in organizers:
+            context['features'].append({
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [elem.coordinates()]
+                },
+                "properties": {
+                    "title": elem.title,
+                    "placeId": elem.id,
+                    "detailsUrl": {
+                        'title': elem.title,
+                        'imgs': [picture.image.url for picture in elem.images.all()],
+                        'description_short': elem.short_description,
+                        'description_long': elem.long_description,
+                        'coordinates': {
+                            'lat': elem.latitude,
+                            'lng': elem.longitude,
+                        },
+                    }
+                }
+            },)
+        print(context)
+        return context
