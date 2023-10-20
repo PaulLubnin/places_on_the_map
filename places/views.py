53f815b325694a406c80b4914d9c5297b2713203
paulego <egopaul@inbox.ru>
2023-10-20 11:36:05 +0500

    Render to template

--- places/views.py
+++ places/views.py
@@ -1,4 +1,4 @@
-from django.http import JsonResponse
+
 from django.views.generic import TemplateView
 
 from places.models import EventOrganizer
@@ -8,12 +8,11 @@ class Index(TemplateView):
     template_name = 'index.html'
 
     def get_context_data(self, **kwargs):
-        context = {}
-        context['type'] = 'FeatureCollection'
-        context['features'] = []
+        context = super().get_context_data(**kwargs)
+        context = {'data': {'type': 'FeatureCollection', 'features': []}}
         organizers = EventOrganizer.objects.all()
         for elem in organizers:
-            context['features'].append({
+            context['data']['features'].append({
                 "type": "Feature",
                 "geometry": {
                     "type": "Point",
