1efa7794fd4713033c7156e0e1ef2109906a74f6
paulego <egopaul@inbox.ru>
2023-10-24 16:24:20 +0500

    Added inlines to AdminEventOrganizer.

--- places/admin.py
+++ places/admin.py
@@ -1,10 +1,25 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
 from .models import EventOrganizer, Image
+
+
+class ImageInline(admin.TabularInline):
+    model = Image
+    fields = ('image', 'get_preview', 'image_order',)
+    readonly_fields = ('get_preview',)
+
+    @admin.display(description='Предпросмотр')
+    def get_preview(self, obj):
+        if not obj:
+            return
+        return format_html(f'<img src="{obj.image.url}" height={200}px/>')
 
 
 @admin.register(EventOrganizer)
 class AdminEventOrganizer(admin.ModelAdmin):
     list_display = ('title',)
+    inlines = (ImageInline,)
 
 
 @admin.register(Image)
